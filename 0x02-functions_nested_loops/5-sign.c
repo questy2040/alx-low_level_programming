9d58bb244574576f367cbf447e051cce3130943d
John Bosco <bosco403@gmail.com>
2022-12-14 11:54:41 +0000

    Display Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
-* print_sign - prints the sign of a number
- * @n: the int to check
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ * print_sign - main function
+ *
+ * @n: character to compare
+ *
+ * Return: 1 if positive, 0 if zero, -1 if negative
+ *
  */
 int print_sign(int n)
 {
@@ -12,14 +13,12 @@ if (n > 0)
 {
 _putchar('+');
 return (1);
-} else if (n == 0)
-{
-_putchar(48);
-return (0);
-} else if (n < 0)
-{
-_putchar('-');
 }
+if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+_putchar('-');
 return (-1);
 }
-
