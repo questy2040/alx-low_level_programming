a766c9e13f6f6bf6d01d68247b893ce29135947b
questy2040 <bosco403@gmail.com>
2022-12-19 17:00:15 +0000

    String and new line

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,16 @@
 #include "main.h"
 #include "2-strlen.c"
 /**
-*_puts - prints a string, followed by a new line, to stdout
+*_puts - puts a string
+*
 * @str: string to print
 */
 void _puts(char *str)
 {
-while (*str != '\0')	
+int i;
+for (i = 0; *(str + i) != '\0'; i++)
 {
-_putchar(*str++);
+_putchar(*(str + i));
 }
 _putchar('\n');
 }
