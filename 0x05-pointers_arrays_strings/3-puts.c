567a8d689595ddd937079912551f606ac5a052d5
questy2040 <bosco403@gmail.com>
2022-12-19 16:47:26 +0000

    String and new line

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* _puts - prints a string, followed by a new line, to stdout
+*_puts - prints a string, followed by a new line, to stdout
 * @str: string to print
 */
 void _puts(char *str)
@@ -11,4 +11,3 @@ _putchar(*str++);
 }
 _putchar('\n');
 }
-
